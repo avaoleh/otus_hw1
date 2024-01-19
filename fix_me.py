5e7bb0b22940699c7722dd7d21a8af52620f0316
AvaOleh <avakimyanov@gmail.com>
2024-01-19 23:35:42 +0300

    HW1: edited fix_me.py according to tips from flake8 and pylint

--- fix_me.py
+++ fix_me.py
@@ -1,8 +1,22 @@
-def calculate_average(nums):
-    total = sum(nums)
+"""Otus HW1"""
+
+
+def calculate_average(numbers):
+    """Summary or Description of the Function
+
+    Parameters:
+    total (float): sum numbers
+    count(int): count numbers
+
+    Returns:
+    average: Returning value
+    """
+    total = sum(numbers)
     count = len(numbers)
     average = total / count
+    return average
+
 
 nums = [10, 15, 20]
-result = calculat_average(nums)
-Print("The average is:", result)
+result = calculate_average(nums)
+print("The average is:", result)
